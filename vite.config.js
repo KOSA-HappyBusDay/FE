60a126d241beafb3a8019eecb760743661de7bab
tjrqh <asdww6631@gmail.com>
2023-12-13 09:28:51 +0900

    vercel deploy

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,7 @@ import { fileURLToPath, URL } from 'node:url';
 import vue from '@vitejs/plugin-vue';
 
 export default {
-  plugins: [
-    vue(),
-    {
-      // Replace axios import with a virtual module
-      name: 'replace-axios',
-      transform: (code, id) => {
-        if (id.includes('node_modules/axios')) {
-          return code.replace(/import\s*[^'"]*['"]axios['"];/g, 'import axios from "virtual-axios";');
-        }
-        return null;
-      },
-    },
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
@@ -25,9 +13,5 @@ export default {
   },
   server: {
     open: true,
-  },
-  define: {
-    // By default, Vite doesn't include shims for NodeJS/
-    // necessary for segment analytics lib to work
   },
 };
